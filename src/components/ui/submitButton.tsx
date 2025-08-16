5b76b5be24e4db60bddc85483dfabe084073eeca
timothymalcham <timothymalcham@gmail.com>
2025-08-16 18:18:46 +0100

    get tanstack form fully working

--- src/components/ui/submitButton.tsx
+++
@@ -1,5 +0,0 @@
-export function SubmitButton() {
-    return (
-        <button type="submit">Submit</button>
-    )
-}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,39 +4,21 @@ import { convexQuery } from '@convex-dev/react-query'
 import { useMutation } from "convex/react";
 import { api } from "~/_generated/api";
 import { z } from 'zod'
-import { createFormHook, createFormHookContexts } from '@tanstack/react-form'
+import { useForm } from '@tanstack/react-form'
 import { Input } from "@/components/ui/input"
-import { SubmitButton } from "@/components/ui/submitButton"
 
 // Tanstack Router - Route Setup
 export const Route = createFileRoute('/')({
     component: Home,
 })
 
-// Tanstack Form - Form Setup
-const { fieldContext, formContext } = createFormHookContexts()
-
-// Allow us to bind components to the form to keep type safety but reduce production boilerplate
-// Define this once to have a generator of consistent form instances throughout your app
-const { useAppForm } = createFormHook({
-    fieldComponents: {
-        Input,
-    },
-    formComponents: {
-        SubmitButton,
-    },
-    fieldContext,
-    formContext,
-})
-
 function Home() {
     // Convex Queries & Mutations
     const todosQuery = useSuspenseQuery(convexQuery(api.todo.getAll, {}))
-    console.log("TODOS: ", todosQuery);
     const createTodoItem = useMutation(api.todo.create);
 
     // Tanstack Form
-    const form = useAppForm({
+    const form = useForm({
         defaultValues: {
             title: ""
         },
@@ -57,20 +39,47 @@ function Home() {
         <div>
             <h2>Add a todo item:</h2>
 
-            <form onSubmit={async (e) => {
-                e.preventDefault()
-                await form.handleSubmit()
-            }}>
-                {/* Components are bound to `form` and `field` to ensure extreme type safety */}
-                {/* Use `form.AppField` to render a component bound to a single field */}
-                <form.AppField
-                    name="title"
-                    children={(field) => <field.Input name="title" />}
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault()
+                    e.stopPropagation()
+                    void form.handleSubmit()
+                }
+            }>
+                <form.Subscribe
+                    selector={(state) => [state.errorMap]}
+                    children={([errorMap]) =>
+                        errorMap.onSubmit ? (
+                            <div>
+                                <em>There was an error on the form: {errorMap.onSubmit.toString()}</em>
+                            </div>
+                        ) : null
+                    }
                 />
-                {/* Components in `form.AppForm` have access to the form context */}
-                <form.AppForm>
-                    <form.SubmitButton />
-                </form.AppForm>
+                <form.Field
+                    name="title"
+                    children={(field) => (
+                        <div>
+                        <Input
+                            name="title"
+                            value={field.state.value}
+                            onChange={(e) => field.handleChange(e.target.value)}
+                        />
+                            {field.state.meta.errors.map((error) => (
+                                error && <p key={error as never as string}>{error.message}</p>
+                            ))}
+                        </div>
+                    )}
+                />
+                <form.Subscribe
+                    selector={(formState) => [formState.canSubmit, formState.isSubmitting]}
+                >
+                    {([canSubmit, isSubmitting]) => (
+                        <button type="submit" disabled={!canSubmit}>
+                            {isSubmitting ? '...' : 'Submit'}
+                        </button>
+                    )}
+                </form.Subscribe>
             </form>
 
 
