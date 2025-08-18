85808071d011d9df93f1b7d22e3872def8dd7078
timothymalcham <timothymalcham@gmail.com>
2025-08-18 17:14:28 +0100

    add Resend OTP password reset util

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -13,6 +13,7 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as ResendOTPPasswordReset from "../ResendOTPPasswordReset.js";
 import type * as auth from "../auth.js";
 import type * as http from "../http.js";
 import type * as todos from "../todos.js";
@@ -26,6 +27,7 @@ import type * as todos from "../todos.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
+  ResendOTPPasswordReset: typeof ResendOTPPasswordReset;
   auth: typeof auth;
   http: typeof http;
   todos: typeof todos;
